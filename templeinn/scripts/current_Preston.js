7d808d368142cf784595f1094d1a959f28157ee7
alechaws <astarofaz@gmail.com>
2019-04-11 13:52:42 -0700

    Upload to GH for testing

---
+++ templeinn/scripts/current_Preston.js
@@ -0,0 +1,18 @@
+var weatherObject2 =  new XMLHttpRequest();
+weatherObject2.open('GET','https://api.openweathermap.org/data/2.5/weather?zip=83440,us&appid=6eac7e39498499a5967dcc07fc168e3f&units=imperial',true);
+
+weatherObject2.send();
+
+weatherObject2.onload = function(){
+    var weatherInfo2= JSON.parse(weatherObject2.responseText)
+    console.log(weatherInfo2);
+
+    document.getElementById('low').innerHTML=weatherInfo2.main.temp_min;
+    document.getElementById('high').innerHTML=weatherInfo2.main.temp_max;
+    document.getElementById('currentTemp').innerHTML=weatherInfo2.main.temp;
+
+    var iconcode = weatherInfo2.weather[0].icon;
+    var icon_path= "http://openweathermap.org/img/w/"+iconcode+".png";
+    document.getElementById('weather_icon').src = icon_path;
+
+}
