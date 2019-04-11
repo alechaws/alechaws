d035f4e478351c6d9057247cbf6cf1d9071cd1b5
alechaws <astarofaz@gmail.com>
2019-04-11 14:21:04 -0700

    updated scripts

--- templeinn/scripts/current_Preston.js
+++ templeinn/scripts/current_Preston.js
@@ -1,5 +1,5 @@
 var weatherObject2 =  new XMLHttpRequest();
-weatherObject2.open('GET','https://api.openweathermap.org/data/2.5/weather?zip=83440,us&appid=6eac7e39498499a5967dcc07fc168e3f&units=imperial',true);
+weatherObject2.open('GET','//api.openweathermap.org/data/2.5/weather?zip=83440,us&appid=6eac7e39498499a5967dcc07fc168e3f&units=imperial',true);
 
 weatherObject2.send();
 
@@ -12,7 +12,7 @@ weatherObject2.onload = function(){
     document.getElementById('currentTemp').innerHTML=weatherInfo2.main.temp;
 
     var iconcode = weatherInfo2.weather[0].icon;
-    var icon_path= "http://openweathermap.org/img/w/"+iconcode+".png";
+    var icon_path= "//openweathermap.org/img/w/"+iconcode+".png";
     document.getElementById('weather_icon').src = icon_path;
 
 }
